fd40702db3dbbb1d5fdfd7f5963e35739cabe43d
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-07-13 08:45:19 +0900

    deleted  path to picture

--- src/aboutMe.js
+++ src/aboutMe.js
@@ -2,7 +2,6 @@ const AboutMe = () => {
   return (
     <>
       <div className="aboutMe"></div>
-      <img src="blob:https://web.telegram.org/3c5f94ec-2d2c-4fc8-97c2-2e97f52b662b" />
     </>
   );
 };
