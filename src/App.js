1d132eeaeda7a01766440e3481c9c145f4bd01c7
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-07-15 05:27:51 +0500

    moved  the logic  from   about  me  component  to  the  app and  deleted the  about  me

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-//link  to  the  resume 
+//link  to  the  resume
 //https://lmpixels.com/demo/breezycv/dark/1/index.html#resume
-import AboutMe   from "./aboutMe";
-
 function App() {
-
-  
-
-return (
+  return (
     <div className="App">
-      <AboutMe />
-    
+      <div className="aboutMe">
+        <div className="backgroundAbout"></div>
+      </div>
     </div>
   );
 }
