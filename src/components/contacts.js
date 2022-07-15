456523eb0f29874a2b906ddfeca4c07641640219
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-07-15 16:40:01 +0500

    made   skelet  for the contact   component

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -1,5 +1,5 @@
 //phone ,  email ,  github  ,  linked id  , telegram
-import { IconName } from "react-icons/ai";
+import { AiOutlineMail } from "react-icons/ai";
 const Contacts = () => {
   return (
     <>
@@ -7,7 +7,14 @@ const Contacts = () => {
         <div className="blackBack"></div>
         <section className="contacts">
           <div className="contactHeader">contact me</div>
-          <article className="contactIconsContainer"></article>
+          <div className="contactIconsContainer">
+            <AiOutlineMail className="emailIcon" />
+            <AiOutlineMail className="emailIcon" />
+            <AiOutlineMail className="emailIcon" />
+            <AiOutlineMail className="emailIcon" />
+            <AiOutlineMail className="emailIcon" />
+            <AiOutlineMail className="emailIcon" />
+          </div>
         </section>
       </div>
     </>
