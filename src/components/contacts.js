c32744b26bb6b4eccef433590aff9ce679ecb896
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-07-15 10:55:26 +0500

    created  template  for  the  rest  of  components

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -1,5 +1,16 @@
 const Contacts = () => {
-  return <>hello contacts</>;
+  return (
+    <>
+      <div className="contactContainer">
+        <div className="blackBack"></div>
+        <div className="contacts">
+          hello world F.F.F.F. (For Four Free Friends) song by Paolo Conte now
+          on JioSaavn. English music album Amazing Game - Instrumental Music.
+          Download song or listen online ...
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Contacts;
