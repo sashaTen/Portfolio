d2315d2005e7a2537076af52eaba16e8ea021a73
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-07-15 05:36:15 +0500

    check  the   job  titles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //link  to  the  resume
 //https://lmpixels.com/demo/breezycv/dark/1/index.html#resume
-
+import JobTitles from "./components/jobTitles";
 function App() {
   return (
     <div className="App">
