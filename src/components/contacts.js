210a9d556b52aef85d8f32bede76c62d174b850e
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-07-17 03:08:45 +0500

    created  urls  for   icons

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -16,25 +16,60 @@ const Contacts = () => {
         <section className="contacts">
           <div className="contactHeader">contact me</div>
           <div className="contactIconsContainer">
-            <CopyToClipboard
-              text="sasha1keshten@gmail.com"
-              onCopy={() => alert(" your  email was Copied")}
-            >
-              <AiOutlineMail className="contactIcon" />
-            </CopyToClipboard>
+            <div>
+              <CopyToClipboard
+                className="contactIcon"
+                text="sasha1keshten@gmail.com"
+                onCopy={() => alert(" your  email was Copied")}
+              >
+                <AiOutlineMail />
+              </CopyToClipboard>
+            </div>
+            <div>
+              <CopyToClipboard
+                className="contactIcon"
+                text="010 7777 7777"
+                onCopy={() => alert(" your  phone number was Copied")}
+              >
+                <AiOutlinePhone />
+              </CopyToClipboard>
+            </div>
 
-            <CopyToClipboard
-              text="010 7777 7777"
-              onCopy={() => alert(" your  phone number was Copied")}
-            >
-              <AiOutlinePhone className="contactIcon" />
-            </CopyToClipboard>
+            <div>
+              <a
+                href="https://github.com/sashaTen"
+                target="_blank"
+                className="tooltip"
+              >
+                <AiOutlineGithub className="contactIcon" />
+              </a>
+            </div>
 
-            <AiOutlineGithub className="contactIcon" />
-
-            <AiFillLinkedin className="contactIcon" />
-            <BsTelegram className="contactIcon" />
-            <BsInstagram className="contactIcon" />
+            <div>
+              <a
+                target="_blank"
+                href="https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile"
+              >
+                <AiFillLinkedin className="contactIcon" />
+              </a>
+            </div>
+            <div>
+              <CopyToClipboard
+                className="contactIcon"
+                text="sasha_keshten"
+                onCopy={() => alert(" your telegram username was Copied")}
+              >
+                <BsTelegram className="contactIcon" />
+              </CopyToClipboard>
+            </div>
+            <div>
+              <a
+                target="_blank"
+                href="https://www.instagram.com/invites/contact/?i=77gjw0tb77r1&utm_content=dn3ldcl"
+              >
+                <BsInstagram className="contactIcon" />
+              </a>
+            </div>
           </div>
         </section>
       </div>
