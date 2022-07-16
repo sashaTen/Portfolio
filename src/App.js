b4292b8c4a6ca8094abd6d7907e226857d2beb87
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-07-17 03:56:36 +0500

    imported  education  component  to  the  main  app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@
 //https://lmpixels.com/demo/breezycv/dark/1/index.html#resume
 import JobTitles from "./components/jobTitles";
 import Contacts from "./components/contacts";
+import Education from "./components/education";
 function App() {
   return (
     <div className="App">
       <JobTitles />
-      <Contacts />
+      <Education />
     </div>
   );
 }
