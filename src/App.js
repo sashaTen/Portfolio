48faf33b50eb36ad9745f69b0acf056dbae4e43e
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-07-17 10:52:57 +0500

    i  created   new  component    called  Skills

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@
 import JobTitles from "./components/jobTitles";
 import Contacts from "./components/contacts";
 import Education from "./components/education";
+import Skills from "./components/skills";
 function App() {
   return (
     <div className="App">
       <JobTitles />
-      <Contacts />
+      <Skills />
     </div>
   );
 }
