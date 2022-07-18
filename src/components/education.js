3fb9eec63842ee33e1d0ca7882b6bd9eda3182cc
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-07-18 23:54:31 +0500

    corrected   indentation    in    education   component

--- src/components/education.js
+++ src/components/education.js
@@ -6,8 +6,10 @@ const Education = () => {
         <h1 className="educationHeader">Education:</h1>
         <div className="educationList">
           <article className="aboutEducation">
-            i studied software engineering in soonchunhyang university and then
-            i transfered to Seoul Cyber university
+            i studied software engineering in "soon
+            <br />
+            chun <br /> hyang"
+            <br /> and then i transfered to Seoul Cyber university
           </article>
           <a target="_blank" href="http://sgee.sch.ac.kr/main/eng_main.php">
             <img
