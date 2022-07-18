8f5fe95bb0650b14ca4947c2183c5b7308925fc0
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-07-19 00:56:27 +0500

    finished  the   skill   component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <JobTitles />
-      <MySkills />
+      <Education />
     </div>
   );
 }
--- src/components/skills.js
+++ src/components/skills.js
@@ -3,6 +3,7 @@
 import { SiRedux } from "react-icons/si";
 import { AiOutlineGithub } from "react-icons/ai";
 import {
+  DiHtml5,
   DiNpm,
   DiSass,
   DiDatabase,
@@ -12,7 +13,52 @@ import {
   DiJavascript1
 } from "react-icons/di";
 const MySkills = () => {
-  return <></>;
+  return (
+    <>
+      <div className=" skillContainer ">
+        <div className="blackBack"></div>
+        <div className="contactHeader">skills</div>
+        <div className="contactIconsContainer skillGrid">
+          <div className="skillNameIcon">
+            <p>html</p>
+            <DiHtml5 className="contactIcon" />
+          </div>
+          <div className="skillNameIcon">
+            <p>css</p>
+            <DiCss3Full className="contactIcon" />
+          </div>
+          <div className="skillNameIcon">
+            <p>javascript</p>
+            <DiJavascript1 className="contactIcon" />
+          </div>
+          <div className="skillNameIcon">
+            <p>react</p>
+            <DiReact className="contactIcon" />
+          </div>
+          <div className="skillNameIcon">
+            <p>redux</p>
+            <SiRedux className="contactIcon" />
+          </div>
+          <div className="skillNameIcon">
+            <p>github</p>
+            <AiOutlineGithub className="contactIcon" />
+          </div>
+          <div className="skillNameIcon">
+            <p>npm</p>
+            <DiNpm className="contactIcon" />
+          </div>
+          <div className="skillNameIcon">
+            <p>sass</p>
+            <DiSass className="contactIcon" />
+          </div>
+          <div className="skillNameIcon">
+            <p>bootstrap</p>
+            <DiBootstrap className="contactIcon" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default MySkills;
