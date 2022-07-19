4b43bdb8ac60503693e468dbb2f28954fcf6e83e
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-07-19 06:50:16 +0500

    added  new  component   related to   projects and  changed  styliing

--- src/components/expirience.js
+++ src/components/expirience.js
@@ -1,5 +1,18 @@
 const Projects = () => {
-  return <>Projects</>;
+  return (
+    <div className="educationContainer">
+      <div className="blackBack"></div>
+      <h1 className="educationHeader">Education:</h1>
+      <div className="educationList">
+        <section className="singleProjectContainer ">
+          <img
+            src="https://wallpaperaccess.com/full/2594886.jpg"
+            className="projectImg"
+          />
+        </section>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
