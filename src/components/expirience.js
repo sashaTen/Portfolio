670a99308299599990924204bc91cb28ed6ac4c0
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-07-27 15:16:09 +0500

    started  making  the  gallery

--- src/components/expirience.js
+++ src/components/expirience.js
@@ -1,7 +1,15 @@
+import image from "../photo/furniture.jpg";
+
 const Projects = () => {
   return (
     <div className="projectContainer">
-      <article className="galleryContainer"></article>
+      <article className="galleryContainer">
+        <div className="projectImg1">
+          <img className="projectImg1" src={image} />
+        </div>
+        <div className="projectImg2"></div>
+        <div className="projectImg3"></div>
+      </article>
     </div>
   );
 };
