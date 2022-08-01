90279f0de28035f78edfce0a3bb2a52be83ee806
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-08-02 00:28:01 +0500

    made  gallery

--- src/components/expirience.js
+++ src/components/expirience.js
@@ -1,10 +1,46 @@
 import image from "../photo/furniture.jpg";
 import image2 from "../photo/furniture1.jpg";
 import image3 from "../photo/furniture2.jpg";
+import { useState } from "react";
+import { MdArrowForwardIos, MdArrowBackIosNew } from "react-icons/md";
 const Projects = () => {
+  const [moveToRight, setMoveToRight] = useState(0);
+
+  /* 
+[0 ,  100  ,  200]
+
+
+*/
+
+  function moveGallery() {
+    setMoveToRight(moveToRight + 100);
+    if (moveToRight > 100) {
+      setMoveToRight(moveToRight - 200);
+    }
+  }
+
   return (
     <div className="projectContainer">
-      <article className="galleryContainer">
+      <button
+        onClick={() => {
+          moveGallery();
+        }}
+        className="galleryBtnRight"
+      >
+        <MdArrowForwardIos />
+      </button>
+      <button
+        onClick={() => {
+          moveGallery();
+        }}
+        className="galleryBtnLeft"
+      >
+        <MdArrowBackIosNew />
+      </button>
+      <article
+        className="galleryContainer"
+        style={{ right: moveToRight + "%" }}
+      >
         <div className="projectImg1">
           <img className="projectImg1" src={image} />
         </div>
