83e905cdd6a93d03b7a6bf49a647f4cd39366632
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-08-01 23:27:36 +0500

    now  i  have t he  gallery  of   the   images

--- src/components/expirience.js
+++ src/components/expirience.js
@@ -1,5 +1,6 @@
 import image from "../photo/furniture.jpg";
-
+import image2 from "../photo/furniture1.jpg";
+import image3 from "../photo/furniture2.jpg";
 const Projects = () => {
   return (
     <div className="projectContainer">
@@ -7,8 +8,12 @@ const Projects = () => {
         <div className="projectImg1">
           <img className="projectImg1" src={image} />
         </div>
-        <div className="projectImg2"></div>
-        <div className="projectImg3"></div>
+        <div className="projectImg2">
+          <img className="projectImg2" src={image2} />
+        </div>
+        <div className="projectImg3" src={image}>
+          <img className="projectImg2" src={image3} />
+        </div>
       </article>
     </div>
   );
