f4a0e22593b3eb1b6378cf4968e98cbc50e89afe
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-08-02 03:41:06 +0500

    now    you  can   by click  change  the  category  of the pictures  deisplayed

--- src/components/expirience.js
+++ src/components/expirience.js
@@ -1,16 +1,21 @@
 import image from "../photo/furniture.jpg";
 import image2 from "../photo/furniture1.jpg";
 import image3 from "../photo/furniture2.jpg";
+import image4 from "../photo/food.jpg";
+import image5 from "../photo/food2.jpg";
+import image6 from "../photo/food3.jpg";
+import image7 from "../photo/Screenshot_20220726-125123_Chrome.jpg";
+import image8 from "../photo/Screenshot_20220726-125137_Chrome.jpg";
+import image9 from "../photo/Screenshot_20220726-125320_Chrome.jpg";
 import { useState } from "react";
 import { MdArrowForwardIos, MdArrowBackIosNew } from "react-icons/md";
 const Projects = () => {
   const [moveToRight, setMoveToRight] = useState(0);
+  const furnitureImg = [image, image2, image3];
+  const foodImgList = [image4, image5, image6];
+  const movieImgList = [image7, image8, image9];
+  const [currentImgList, setcurrentImgList] = useState(furnitureImg);
 
-  /* 
-[0 ,  100  ,  200]
-
-
-*/
   function moveGalleryToLeft() {
     setMoveToRight(moveToRight - 100);
     if (moveToRight < 100) {
@@ -27,6 +32,13 @@ const Projects = () => {
 
   return (
     <div className="projectContainer">
+      <button
+        onClick={() => {
+          setcurrentImgList(foodImgList);
+        }}
+      >
+        press
+      </button>
       <button
         onClick={() => {
           moveGalleryToRight();
@@ -50,13 +62,13 @@ const Projects = () => {
         style={{ right: moveToRight + "%" }}
       >
         <div className="projectImg1">
-          <img className="projectImg1" src={image} />
+          <img className="projectImg1" src={currentImgList[0]} />
         </div>
         <div className="projectImg2">
-          <img className="projectImg2" src={image2} />
+          <img className="projectImg2" src={currentImgList[1]} />
         </div>
-        <div className="projectImg3" src={image}>
-          <img className="projectImg2" src={image3} />
+        <div className="projectImg3">
+          <img className="projectImg2" src={currentImgList[2]} />
         </div>
       </article>
     </div>
