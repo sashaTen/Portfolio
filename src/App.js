24e2f47560f3cdebb2c3d3adeda480447c1f7087
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-08-11 12:38:10 +0500

    created   some   basic  functionality of how to  switch   components

--- src/App.js
+++ src/App.js
@@ -5,11 +5,76 @@ import Contacts from "./components/contacts";
 import AboutMe from "./components/about";
 import MySkills from "./components/skills";
 import Projects from "./components/expirience";
+import { useState } from "react";
+
+/*
+
+  <AboutMe/>
+      <MySkills/>
+      <Contacts/>
+      <Projects/>
+*/
 
 function App() {
+  const [skills, setSkills] = useState(false);
+  const [contacts, SetContacts] = useState(false);
+
+  const [expirience, SetExpirience] = useState(false);
+
+  const [education, SetEducation] = useState(false);
+
+  let listOfDisplayedComponenst = [
+    setSkills,
+    SetContacts,
+    SetEducation,
+    SetExpirience
+  ];
+
   return (
     <div className="App">
-      <Projects />
+      <div className="menuHeader">
+        <button
+          onClick={() => {
+            SetEducation(!education);
+          }}
+          className="menuBtn"
+        >
+          about me
+        </button>
+        <button
+          onClick={() => {
+            setSkills(!skills);
+          }}
+          className="menuBtn"
+        >
+          {" "}
+          my skills
+        </button>
+        <button
+          onClick={() => {
+            SetExpirience(!expirience);
+          }}
+          className="menuBtn"
+        >
+          {" "}
+          my projects
+        </button>
+        <button
+          onClick={() => {
+            SetContacts(!contacts);
+          }}
+          className="menuBtn"
+        >
+          {" "}
+          contacts
+        </button>
+      </div>
+
+      <JobTitles />
+      {contacts && <Contacts />}
+      {skills && <MySkills />}
+      {expirience && <Projects />}
+      {education && <AboutMe />}
     </div>
   );
 }
