c0856ce770db87d65bfdf655769de11759cbd1d8
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-08-21 15:55:23 +0900

    added    animated  text  to the  jobtitles  component

--- src/components/jobTitles.js
+++ src/components/jobTitles.js
@@ -1,14 +1,25 @@
-const JobTitles = (props) => {
-  const blurred = props.blurState;
+import { useState } from "react";
 
+const JobTitles = (props) => {
+  const [textEffectClass, SETtextEffectClass] = useState({});
+  const blurred = props.blurState;
+  const effectAttributes = {
+    opacity: "1",
+    position: "relative",
+    left: "0%"
+  };
+
+  setTimeout(() => {
+    SETtextEffectClass(effectAttributes);
+  }, 500);
   //    filter: blur(15px);
   return (
     <div className="mainContainer">
       <div className="jobTitleContainer" style={blurred}>
-        <h1>hello </h1>
-        <h1>i am ten alexander</h1>
-        <h1>i am web developer</h1>
-        <h1>and ux/ui designer</h1>
+        <h1 style={textEffectClass}>hello </h1>
+        <h1 style={textEffectClass}>i am ten alexander</h1>
+        <h1 style={textEffectClass}>i am web developer</h1>
+        <h1 style={textEffectClass}>and ux/ui designer</h1>
       </div>
     </div>
   );
