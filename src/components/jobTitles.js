60d6e932bf4a0bba0890cbc6dfc0d6f687a92999
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-08-22 20:59:32 +0900

    now the  tet  in   job  titles   appears    one   by    one

--- src/components/jobTitles.js
+++ src/components/jobTitles.js
@@ -2,6 +2,10 @@ import { useState } from "react";
 
 const JobTitles = (props) => {
   const [textEffectClass, SETtextEffectClass] = useState({});
+  const [textEffectClass2, SETtextEffectClass2] = useState({});
+  const [textEffectClass3, SETtextEffectClass3] = useState({});
+  const [textEffectClass4, SETtextEffectClass4] = useState({});
+
   const blurred = props.blurState;
   const effectAttributes = {
     opacity: "1",
@@ -9,17 +13,30 @@ const JobTitles = (props) => {
     left: "0%"
   };
 
-  setTimeout(() => {
-    SETtextEffectClass(effectAttributes);
-  }, 500);
+  function displayText(time) {
+    setTimeout(() => {
+      SETtextEffectClass(effectAttributes);
+    }, 500);
+    setTimeout(() => {
+      SETtextEffectClass2(effectAttributes);
+    }, 700);
+    setTimeout(() => {
+      SETtextEffectClass3(effectAttributes);
+    }, 1000);
+    setTimeout(() => {
+      SETtextEffectClass4(effectAttributes);
+    }, 1200);
+  }
+  displayText();
+
   //    filter: blur(15px);
   return (
     <div className="mainContainer">
       <div className="jobTitleContainer" style={blurred}>
         <h1 style={textEffectClass}>hello </h1>
-        <h1 style={textEffectClass}>i am ten alexander</h1>
-        <h1 style={textEffectClass}>i am web developer</h1>
-        <h1 style={textEffectClass}>and ux/ui designer</h1>
+        <h1 style={textEffectClass2}>i am ten alexander</h1>
+        <h1 style={textEffectClass3}>i am web developer</h1>
+        <h1 style={textEffectClass4}>and ux/ui designer</h1>
       </div>
     </div>
   );
